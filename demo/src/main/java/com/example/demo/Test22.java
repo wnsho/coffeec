280eba401986edd0955cd0e7abcd8a66c7e3fca5
wnsho <jhkimm00@naver.com>
2021-06-17 20:20:31 +0900

    test2

---
+++ demo/src/main/java/com/example/demo/Test22.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Test22 {
+
+	public static void main(String[] args) {
+	}
+	
+	
+}
